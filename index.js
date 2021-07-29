3c2e6e046ccd0eef13e759da36d32902e290e349
taylortreece <tayloratreece@gmail.com>
2021-07-29 13:38:28 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Add your code here
+const configurationObject = {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+        'Application': 'application/json'
+    },
+    body: JSON.stringify({
+        name: "Taylor",
+        email: "tayloratreece@gmail.com"
+    })
+}
+
+const dogUrl = 'http://localhost:3000/dogs'
+const catUrl = 'http://localhost:3000/cats'
+const userUrl = 'http://localhost:3000/users'
+
+function submitData(url, opts) {
+    fetch(url, opts);
+}
+
+submitData(userUrl, configurationObject)
