2722b5b33f8bb21dbc4c46783a109eb5d1c9fca4
taylortreece <tayloratreece@gmail.com>
2021-07-29 15:18:08 -0500

    Done.

--- index.js
+++ index.js
@@ -1,21 +1,42 @@
-const configurationObject = {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json',
-        'Application': 'application/json'
-    },
-    body: JSON.stringify({
-        name: "Taylor",
-        email: "tayloratreece@gmail.com"
-    })
-}
-
 const dogUrl = 'http://localhost:3000/dogs'
 const catUrl = 'http://localhost:3000/cats'
-const userUrl = 'http://localhost:3000/users'
 
-function submitData(url, opts) {
-    fetch(url, opts);
+function submitData(name, email) {
+    const userUrl = 'http://localhost:3000/users'
+
+    const configurationObject = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({
+            name: `${name}`,
+            email: `${email}`
+        })
+    }
+
+    return fetch(userUrl, configurationObject)
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(object) {
+        console.log(object)
+        console.log(object.id)
+        appendTextToBody(object.id)
+      })
+      .catch(function(error) {
+          alert("U aren't supposed to be here bruh");
+          console.log(error.message);
+      }); 
 }
 
-submitData(userUrl, configurationObject)
+// function appendTextToBody(text) {
+//     let newElement = document.createElement('p');
+//     newElement.innerText = text
+//     document.body.appendChild(newElement)
+// }
+
+function appendTextToBody(text) {
+    document.body.innerHTML = text
+}
